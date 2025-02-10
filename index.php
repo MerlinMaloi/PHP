47ed9b07b84df4f24eeab604c12fcc9d49f4b046
MerlinMaloi <159427589+MerlinMaloi@users.noreply.github.com>
2025-02-11 00:48:13 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<!-- это «голова» страницы -->
+<head>
+</head>
+
+<!-- это «тело» страницы -->
+<body>
+<?php
+
+echo "Привет, мир!";?><br> <?php
+echo "Hello, World with echo!";?><br><?php
+print "Hello, World with print!";
+$y ="" ;
+$days = 288;
+echo $y, $days; ?><br><?php
+$message = "Все возвращаются на работу!";
+echo $days; ?><br><?php
+echo $message; ?><br><?php
+$gu = $message . $days ;?><br><?php
+echo $gu;?><br><?php
+
+?>
+</body>
+</html>
+
